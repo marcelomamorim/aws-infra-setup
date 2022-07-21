b56ebd14a7c5d48b99adc66c05603326ef6e2cac
marcelo amorim <marcelo.med.amorim@gmail.com>
2022-07-20 23:15:39 -0300

    teste

--- src/main/java/br/com/awsinfrasetup/AwsInfraSetupApplication.java
+++ src/main/java/br/com/awsinfrasetup/AwsInfraSetupApplication.java
@@ -3,6 +3,9 @@ package br.com.awsinfrasetup;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * teste
+ */
 @SpringBootApplication
 public class AwsInfraSetupApplication {
 
